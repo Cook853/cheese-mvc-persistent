e6f374b76ff693a21f7b10f2094708cb073d097f
Andrew Cook <andrew.scott.cook.law@gmail.com>
2017-04-10 23:51:51 -0500

    Added category view

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,9 +1,12 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Cheese;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Lauren on 4/10/2017.
@@ -15,5 +18,15 @@ public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    // Request path: /category
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Categories");
+
+        return "cheese/index";
+    }
 
 }
