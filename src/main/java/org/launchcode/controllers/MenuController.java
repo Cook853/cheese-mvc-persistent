f7db2c62127a6c6ee6727f65392683c8fe23d17b
Andrew Cook <andrew.scott.cook.law@gmail.com>
2017-04-12 22:19:40 -0500

    Finished part 3, pre-test

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,9 +1,10 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Category;
+import org.launchcode.models.Cheese;
 import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
+import org.launchcode.models.forms.AddMenuItemForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -69,6 +70,40 @@ public class MenuController {
         model.addAttribute("title", menu.getName());
 
         return "menu/view";
+    }
+
+    @RequestMapping(value = "add-item/{id}", method = RequestMethod.GET)
+    public String displayAddMenuItemForm(@PathVariable int id, Model model) {
+
+        Menu menu = menuDao.findOne(id);
+
+        AddMenuItemForm form = new AddMenuItemForm(menu, cheeseDao.findAll());
+
+        model.addAttribute("form", form);
+        model.addAttribute("cheeses", cheeseDao.findAll());
+        model.addAttribute("menu", menu);
+        model.addAttribute("title", "Add item to menu: " + menu.getName());
+
+        return "menu/add-item";
+    }
+
+    @RequestMapping(value = "add-item/{id}", method = RequestMethod.POST)
+    public String processAddMenuItemForm(Model model,
+                                     @ModelAttribute @Valid AddMenuItemForm form,
+                                         Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add item to menu: " +
+                    menuDao.findOne(form.getMenuId()).getName());
+            return "menu/add-item";
+        }
+
+        Menu menu = menuDao.findOne(form.getMenuId());
+        Cheese cheese = cheeseDao.findOne(form.getCheeseId());
+        menu.addItem(cheese);
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
     }
 
 }
